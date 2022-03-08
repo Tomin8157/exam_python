1617619685770391080c9d7a2564a32df3de349c
Tomin8157 <94525397+Tomin8157@users.noreply.github.com>
2022-03-08 12:43:57 +0530

    exam

---
+++ FUNCTION.py
@@ -0,0 +1,11 @@
+def compare(s1, s2, n):
+    if s1 == s2:
+        print("True")
+    else:
+        print("false")
+
+
+a = input("enter the string 1 : ")
+b = input("enter the string 2 : ")
+
+compare(a, b, 3)
---
+++ area.py
@@ -0,0 +1,20 @@
+# Area of Triangle
+print("TRIANGLE")
+b = float(input("enter the breadth of Triangle: "))
+h = float(input("enter the height of Triangle: "))
+area = 1/2 * b * h
+print("area of triangle with breadth", b, "and height", h, "is: ", area, "\n \n")
+
+# Area of Rectangle
+print("RECTANGLE")
+l = float(input("enter the length of Rectangle: "))
+b = float(input("enter the breadth of Rectangle: "))
+area = l * b
+print("The area of Rectangle is : ", area, "\n\n")
+
+# Area of Circle
+print("CIRCLE")
+r = float(input("enter the radius of Circle : "))
+pi = 3.14
+area = pi * r * r
+print("The area of circle is : ", area)
